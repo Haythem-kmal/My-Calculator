620ace8ecb3d7827124f9e8effe70c6133c66ea9
Haythem-kmal <36045493+Haythem-kmal@users.noreply.github.com>
2018-02-04 21:20:42 +0200

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,7 +1,7 @@
 /**
  * IMPORTANT: Make sure you are using the correct package name.
  * This example uses the package name:
- * package com.example.android.justjava
+ * package com.example.android.calculator
  * If you get an error when copying this code into Android studio, update it to match teh package name found
  * in the project's AndroidManifest.xml file.
  **/
